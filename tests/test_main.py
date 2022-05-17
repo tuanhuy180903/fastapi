bebb69e41ad4ddc595f3f04b2bd5cb43a0c3065c
tuanhuy180903 <lehuutuanhuy@gmail.com>
2022-05-17 17:43:29 +0700

    update test file

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,24 +1,18 @@
-import pytest
-
-from httpx import AsyncClient
+from starlette.testclient import TestClient
 
 import sys, os
 sys.path.append(os.path.abspath(os.path.join('..', 'fastapi')))
 print(sys.path)
 from main import app
 
-#@pytest.mark.anyio
-@pytest.mark.asyncio
-async def test_root():
-    async with AsyncClient(app=app, base_url="http://127.0.0.1:8000") as ac:
-        response = await ac.get("/")
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Data": "Hello!"}
 
-async def test_fleet():
-    async with AsyncClient(app=app, base_url="http://127.0.0.1:8000/fleet") as ac:
-        response = await ac.get("/2")
+def test_fleet():
+    response = client.get("/fleet/2")
     assert response.status_code == 404
     assert response.json() == {"detail": "Fleet not found"}
-
-
